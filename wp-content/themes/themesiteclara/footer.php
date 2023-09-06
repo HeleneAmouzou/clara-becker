f4d74078fb5598acd99be79a670b5062b6002f1e
HeleneAmouzou <helene.amouzou@gmail.com>
2023-09-06 20:58:33 +0200

    feat: header and footer

--- wp-content/themes/themesiteclara/footer.php
+++ wp-content/themes/themesiteclara/footer.php
@@ -1,6 +1,11 @@
 </div> 
-<?php wp_footer() ?>
-   
 </div>
 </body>
+<?php wp_footer() ?>
+    <div class="footer-content">
+        <p>CVG</p>
+        <p>Conditions Générales d'Utilisation</p>
+        <p>Copyright <span><?php echo date("Y"); ?></span> Gwenaëlle Nicolas & Hélène Amouzou</p>
+    </div>
+
 </html>
--- wp-content/themes/themesiteclara/header.php
+++ wp-content/themes/themesiteclara/header.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clara Becker</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
     <?php wp_head() ?>
 </head>
-<body> 
+
     <header>
-        <?php
-            $upload_dir = wp_upload_dir();
-            $image_path = $upload_dir['baseurl'] . '/2023/08/IMG-1079-150x150.png';
-        ?>
-        <img src="<?php echo $image_path; ?>" alt="Image en-tête">
+        <div class="logo">
+            <?php
+                $upload_dir = wp_upload_dir();
+                $image_path = $upload_dir['baseurl'] . '/2023/08/IMG-1079-150x150.png';
+            ?>
+            <img src="<?php echo $image_path; ?>" alt="Illustration abstraite cercles de différentes couleurs en en-tête">
+            <h1>Clara Becker</h1>
+        </div>
     </header>
+
+    <body> 
     <div class="container">
         <nav>
                 <?php wp_nav_menu([
