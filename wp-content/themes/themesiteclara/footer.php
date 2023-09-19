bb34afb23a63b24274a961a737727edbd532eab4
HeleneAmouzou <helene.amouzou@gmail.com>
2023-09-19 22:44:05 +0200

    refactoring code

--- wp-content/themes/themesiteclara/footer.php
+++ wp-content/themes/themesiteclara/footer.php
@@ -1,10 +1,11 @@
-</div> 
-</div>
+    </div> 
+    </div>
 </body>
+
 <?php wp_footer() ?>
-<div class="footer-content">
+    <div class="footer-content">
         <p>CVG</p>
-        <p>Conditions Générales d'Utilisation</p>
+        <p>CGU</p>
         <p>Copyright <span><?php echo date("Y"); ?></span> Gwenaëlle Nicolas & Hélène Amouzou</p>
     </div>
-</html>
+</htmclara
--- wp-content/themes/themesiteclara/header.php
+++ wp-content/themes/themesiteclara/header.php
@@ -11,22 +11,22 @@
     <?php wp_head() ?>
 </head>
 <header>
-        <div class="logo">
-            <?php
-                $upload_dir = wp_upload_dir();
-                $image_path = $upload_dir['baseurl'] . '/2023/08/IMG-1079-150x150.png';
-            ?>
-            <img src="<?php echo $image_path; ?>" alt="Illustration abstraite cercles de différentes couleurs en en-tête">
-            <h1>Clara Becker</h1>    
-        </div>
-    </header>
-    <body> 
+    <div class="logo">
+        <?php
+            $upload_dir = wp_upload_dir();
+            $image_path = $upload_dir['baseurl'] . '/2023/08/IMG-1079-150x150.png';
+        ?>
+        <img src="<?php echo $image_path; ?>" alt="Illustration abstraite cercles de différentes couleurs en en-tête">
+        <h1>Clara Becker</h1>    
+    </div>
+</header>
+<body> 
     <div class="container">
         <nav>
-                <?php wp_nav_menu([
-                    'theme_location' => 'header',
-                    'container' => 'nav',
-                    'container_class' => 'vertical-menu'
-                    ]) ?>
+            <?php wp_nav_menu([
+                'theme_location' => 'header',
+                'container' => 'nav',
+                'container_class' => 'vertical-menu'
+                ]) ?>
         </nav>
     <div class="content">        
